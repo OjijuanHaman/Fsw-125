52ea168a1331a57697cbf5451904495d41100ee7
OjijuanHaman <ojijuan.haman@bryanuniversity.edu>
2021-08-08 15:31:14 -0500

    added week1 assignments

---
+++ week1/app.js
@@ -0,0 +1,8 @@
+const mathFormulas = require('./math')
+
+console.log(mathFormulas)
+
+console.log(mathFormulas.sum(2,2));
+console.log(mathFormulas.substract(20,15));
+console.log(mathFormulas.multiply(2,3));
+console.log(mathFormulas.divide(70,10));
---
+++ week1/math.js
@@ -0,0 +1,22 @@
+const sum = (a,b) => {
+    return a + b
+};
+
+const substract = (c,d) => {
+    return c - d
+};
+
+const multiply = (e,f) => {
+    return e * f
+};
+
+const divide = (g,h) => {
+    return g / h
+};
+
+module.exports = {
+    sum,
+    substract,
+    multiply,
+    divide
+}
